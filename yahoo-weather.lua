2300c9fffc5d4a2566f7cd72201c8947dcd209eb
Sheda <sheda1805@gmail.com>
2015-12-05 16:35:26 +0100

    [YAHOO-WEA] Cosmetic and use global value

--- yahoo-weather.lua
+++ yahoo-weather.lua
@@ -1,17 +1,21 @@
 local httpClient = require "http-client"
---local globals = require "globals"
-local url = "https://query.yahooapis.com/v1/public/yql?q=";
-      url = url.."select%20item.condition%20from%20weather.forecast%20where%20woeid%3D576272";
-      url = url.."%20and%20u%3D'c'&format=json";
+local g = require "globals"
+
+local url = "http://query.yahooapis.com/v1/public/yql?q=";
+      url = url.."select%20item.condition%20from%20weather.forecast%20where%20woeid%"..g.woeid;
+      url = url.."%20and%20u%3D%27c%27&format=json";
 local y = {}
 function y.fetch(callback)
     httpClient.getJson(url, function(json) 
-        local Temp = json.query.results.channel.item.condition.temp
-        local Code = json.query.results.channel.item.condition.code
-        local Text = json.query.results.channel.item.condition.text
+        local Temp = json.query.results.channel.item.condition.temp;
+        local Code = json.query.results.channel.item.condition.code;
+        local Text = json.query.results.channel.item.condition.text;
         json = nil
-        collectgarbage()
-        callback(Temp, Code, Text)
+        print ("Temp"..Temp);
+        print ("Code"..Code);
+        print ("Text"..Text);
+        collectgarbage();
+        callback(Temp, Code, Text);
     end)
 end
 return y
