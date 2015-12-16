a9e8612b738d43f64f5a02bc4178e50ad9a3e3ac
Sheda <sheda1805@gmail.com>
2015-12-16 23:11:44 +0100

    [GLOBAL] Add pos poke

--- globals.lua
+++ globals.lua
@@ -1,9 +1,10 @@
 local globals = {
     woeid = "3D576272",    -- Antibes - http://woeid.rosselliot.co.nz/lookup/
+    pos_poke = 35,         -- Poke position
     poscnt = {value = 0},  -- If not Zere will keep current position
     led = {value = 0,      -- 0 - 1
            pin = 4},       -- GPIO2
-    servo = { value = 512, -- range from 0-1023
+    servo = { value = 74, -- range from 0-1023
               pin = 3}     -- GPIO0
 }
 return globals
